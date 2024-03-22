6ac6652c65965aedb8cf5592c2f8ebf94a2923bf
Adel Müürsepp <adel.muursepp@gmail.com>
2024-03-22 15:37:39 -0400

    Added the camera page view

--- src/components/camera/index.jsx
+++ src/components/camera/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import NavBar from "../shared/NavBar";
 
-function ARPage() {
-  return <div>Test</div>;
-}
+const ARScene = () => {
+  return (
+    <div>
+      <a-scene
+        vr-mode-ui="enabled: false"
+        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
+        renderer="antialias: true; alpha: true"
+      >
+        <a-camera gps-new-camera="gpsMinDistance: 5; gpsAccuracy: 500"></a-camera>
+        <a-entity
+          material="color: blue"
+          geometry="primitive: sphere"
+          gps-new-entity-place="latitude: 43.64345853120786; longitude: -79.38229042530689"
+          scale="5 5 5"
+        ></a-entity>
+      </a-scene>
+      <NavBar />
+    </div>
+  );
+};
 
-export default ARPage;
+export default ARScene;
