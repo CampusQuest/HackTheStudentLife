98add6ca2a76a460d386419492da262034c025b0
Min Jae David Park <mjdavid.park@mail.utoronto.ca>
2024-03-22 13:49:48 -0400

    Update navbar

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import "./NavBar.css";
+import home from "../../assets/home.png";
+import camera from "../../assets/camera.png";
+import calendar from "../../assets/calendar.png";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-items">
-        <li className="nav-item">{/* <a href="/">Home</a> */}</li>
-        <li className="nav-item">{/* <a href="/camera">Camera</a> */}</li>
-        <li className="nav-item">{/* <a href="/events">Events</a> */}</li>
+        <li className="nav-item">
+          <Link to='/'>
+            <img src={home} alt="home" />
+          </Link></li>
+        <li className="nav-item">
+          <Link to='/camera'>
+            <img src={camera} alt="camera" />
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to='/events'>
+            <img src={calendar} alt="calendar" />
+          </Link></li>
       </ul>
     </nav>
   );
